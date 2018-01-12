006cba920eee8a09690316b68363de7977ef72d6
Александр <shurik.box@gmail.com>
2018-01-12 21:33:47 +0300

    complete js

--- js/main.js
+++ js/main.js
@@ -5,38 +5,59 @@ const BODY = document.body;
 // Remove class 'no-js'
 BODY.classList.remove('no-js');
 
-// Create New Element
-function el(tagName, attributes, children) {
-  const element = document.createElement(tagName);
+;(function () {
+  const numList = [7, 4];
+  const total = numList.reduce((sum, num) => sum + num, 0);
+  const DECIMAL_WIDTH = 39;
+  const decimalsList = document.querySelector('.b-rule_decimals');
+  const axisWrap = document.querySelector('.b-rule_axis');
+  const exampleTotal = document.querySelector('.b-example_total');
+  const exampleNums = Array.from(document.querySelectorAll('.b-example_num'));
+  const completeList = [];
 
-  if (typeof attributes === 'object') {
-    Object.keys(attributes).forEach(i => element.setAttribute(i, attributes[i]));
-  }
+  numList.forEach((num, i) => {
+    const segment = renderSegment(num, i);
+    const segmentInput = segment.querySelector('.b-segment_input');
 
-  if (typeof children === 'string') {
-    element.textContent = children;
-  } else if (children instanceof Array) {
-    children.forEach(child => element.appendChild(child));
-  }
+    segmentInput.addEventListener('input', (e) => {
+      const $this = e.target;
 
-  return element;
-}
+      if (parseInt($this.value) === num) {
+        $this.classList.remove('is-error');
+        $this.setAttribute('disabled', '');
 
-class Ruler {
-  constructor(nums = []) {
-    this.nums = nums;
-    this.total = nums.reduce((sum, num) => sum + num, 0);
-    this.decimalSize = 39;
-    this.decimalsList = document.querySelector('.b-rule_decimals');
-    this.axisWrap = document.querySelector('.b-rule_axis');
-    this.exampleTotal = document.querySelector('.b-example_total');
-    this.exampleNums = Array.from(document.querySelectorAll('.b-example_num'));
-  }
+        exampleNums[i].classList.remove('is-current');
 
-  renderSegment(num, prevNum, i) {
+        completeList.push(num);
+
+        if (completeList.length === numList.length) {
+          exampleTotal.removeAttribute('disabled');
+          exampleTotal.value = '';
+
+          exampleTotal.addEventListener('input', (e) => {
+            const $this = e.target;
+
+            if (parseInt($this.value) === total) {
+              $this.classList.remove('is-error');
+              $this.setAttribute('disabled', '');
+            } else {
+              $this.classList.add('is-error');
+            }
+          });
+        }
+      } else {
+        $this.classList.add('is-error');
+        exampleNums[i].classList.add('is-current');
+      }
+    });
+  });
+
+  // Render Segment
+  function renderSegment(num, index) {
+    const prevNum = numList[index - 1];
     const segmentParent = el('div', {
       class: 'b-rule_segment',
-      style: `width: ${this.decimalSize * num}px; left: ${this.decimalSize * (prevNum === undefined ? 0 : prevNum)}px`
+      style: `width: ${DECIMAL_WIDTH * num}px; left: ${DECIMAL_WIDTH * (prevNum === undefined ? 0 : prevNum)}px`
     }, [
       el('input', {
         class: 'b-segment_input',
@@ -46,31 +67,25 @@ class Ruler {
       el('div', { class: 'b-segment_arrow' })
     ]);
 
-    setTimeout(() => this.axisWrap.insertBefore(segmentParent, this.decimalsList), 2000);
+    setTimeout(() => axisWrap.insertBefore(segmentParent, decimalsList), 2000);
 
     return segmentParent;
   }
 
-  checkSegment(segment, num, i) {
-    const segmentInput = segment.querySelector('.b-segment_input');
+  // Create New Element
+  function el(tagName, attributes, children) {
+    const element = document.createElement(tagName);
 
-    segmentInput.addEventListener('input', (e) => {
-      const $this = e.target;
-      if (parseInt($this.value) === num) {
-        $this.classList.remove('is-error');
-        $this.setAttribute('disabled', '');
-        this.exampleNums[i].classList.remove('is-current');
-      } else {
-        $this.classList.add('is-error');
-        this.exampleNums[i].classList.add('is-current');
-      }
-    });
+    if (typeof attributes === 'object') {
+      Object.keys(attributes).forEach(i => element.setAttribute(i, attributes[i]));
+    }
+
+    if (typeof children === 'string') {
+      element.textContent = children;
+    } else if (children instanceof Array) {
+      children.forEach(child => element.appendChild(child));
+    }
+
+    return element;
   }
-}
-
-const ruler = new Ruler([7, 4]);
-
-ruler.nums.forEach((num, i) => {
-  const segment = ruler.renderSegment(num, ruler.nums[i - 1], i);
-  ruler.checkSegment(segment, num, i);
-});
+})();
